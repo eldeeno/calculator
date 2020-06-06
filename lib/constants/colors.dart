40e54497ba0ab88131c2b71a7b5f44ee791755aa
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2020-06-06 08:23:14 +0100

    Adjusted buttons

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 Color kDarkBackground = Colors.black;
 Color kLightColor = Colors.grey;
+Color kButtonColor1 = Color(0xFF363433);
+Color kButtonColor2 = Colors.lightGreen;
--- lib/main.dart
+++ lib/main.dart
@@ -37,217 +37,127 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                Container(
-                  height: 80.0,
-                  padding: EdgeInsets.only(
-                    right: 20.0,
-                    left: 20.0,
-                  ),
-                  child: TextField(
-                    textDirection: TextDirection.rtl,
-                    cursorColor: Colors.transparent,
-                    style: TextStyle(
-                      fontSize: 45.0,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white,
+            Container(
+              height: 80.0,
+              padding: EdgeInsets.only(
+                right: 20.0,
+                left: 20.0,
+              ),
+              child: TextField(
+                textDirection: TextDirection.rtl,
+                cursorColor: Colors.transparent,
+                style: TextStyle(
+                  fontSize: 45.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            Divider(
+              height: 150,
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(right: 30.0, left: 20.0),
+                    child: FaIcon(
+                      FontAwesomeIcons.clock,
+                      color: Colors.white70,
+                      size: 18,
                     ),
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(right: 30.0, left: 10.0),
+                    child: FaIcon(
+                      FontAwesomeIcons.rulerHorizontal,
+                      color: Colors.white70,
+                      size: 18,
                     ),
                   ),
-                ),
-                Container(
-                  height: 120,
-                ),
-                Container(
-                  padding: EdgeInsets.only(bottom: 25.0),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.only(right: 30.0, left: 20.0),
-                        child: FaIcon(
-                          FontAwesomeIcons.clock,
-                          color: Colors.white70,
-                          size: 18,
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(right: 30.0, left: 10.0),
-                        child: FaIcon(
-                          FontAwesomeIcons.rulerHorizontal,
-                          color: Colors.white70,
-                          size: 18,
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(right: 30.0, left: 10.0),
-                        child: FaIcon(
-                          FontAwesomeIcons.simCard,
-                          color: Colors.white70,
-                          size: 18,
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(right: 30.0, left: 120.0),
-                        child: FaIcon(
-                          FontAwesomeIcons.windowClose,
-                          color: Colors.lightGreenAccent,
-                          size: 18,
-                        ),
-                      ),
-                    ],
+                  Container(
+                    padding: EdgeInsets.only(right: 30.0, left: 10.0),
+                    child: FaIcon(
+                      FontAwesomeIcons.simCard,
+                      color: Colors.white70,
+                      size: 18,
+                    ),
                   ),
-                ),
-                Divider(
-                  color: Colors.white70,
-                  thickness: 1,
-                ),
-                Container(
-                  child: Wrap(
-                    spacing: 3.0,
-                    runSpacing: 1.5,
-                    children: <Widget>[
-                      FlatButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        onPressed: () {},
-                        color: Colors.grey,
-                        textColor: Colors.white,
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            height: 1.2,
-                          ),
-                        ),
-                      ),
-                      FlatButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        onPressed: () {},
-                        color: Colors.grey,
-                        textColor: Colors.white,
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            height: 1.2,
-                          ),
-                        ),
-                      ),
-                      FlatButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        onPressed: () {},
-                        color: Colors.grey,
-                        textColor: Colors.white,
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            height: 1.2,
-                          ),
-                        ),
-                      ),
-                      FlatButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        onPressed: () {},
-                        color: Colors.grey,
-                        textColor: Colors.white,
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            height: 1.2,
-                          ),
-                        ),
-                      ),
-                      FlatButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        onPressed: () {},
-                        color: Colors.grey,
-                        textColor: Colors.white,
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            height: 1.2,
-                          ),
-                        ),
-                      ),
-                      FlatButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        onPressed: () {},
-                        color: Colors.grey,
-                        textColor: Colors.white,
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            height: 1.2,
-                          ),
-                        ),
-                      ),
-                      FlatButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        onPressed: () {},
-                        color: Colors.grey,
-                        textColor: Colors.white,
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            height: 1.2,
-                          ),
-                        ),
-                      ),
-                      FlatButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        onPressed: () {},
-                        color: Colors.grey,
-                        textColor: Colors.white,
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            height: 1.2,
-                          ),
-                        ),
-                      ),
-                      FlatButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        onPressed: () {},
-                        color: Colors.grey,
-                        textColor: Colors.white,
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            height: 1.2,
-                          ),
-                        ),
-                      ),
-                    ],
+                  Container(
+                    padding: EdgeInsets.only(right: 30.0, left: 120.0),
+                    child: FaIcon(
+                      FontAwesomeIcons.windowClose,
+                      color: Colors.lightGreenAccent,
+                      size: 18,
+                    ),
                   ),
-                )
-              ],
+                ],
+              ),
+            ),
+            Divider(
+              color: Colors.white70,
+              thickness: 1,
+            ),
+            Container(
+              child: Wrap(
+                spacing: 10.0,
+                runSpacing: 5.0,
+                children: <Widget>[
+                  Container(
+                    height: 65,
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                        side: BorderSide(
+                          width: 0.3,
+                          color: Colors.white30,
+                        ),
+                      ),
+                      onPressed: () {},
+                      color: kButtonColor1,
+                      textColor: Colors.white,
+                      child: Text(
+                        "C",
+                        style: TextStyle(
+                          color: Colors.orangeAccent,
+                          fontSize: 25.0,
+                          fontFamily: 'Arial',
+                          height: 1.2,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 65,
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                        side: BorderSide(
+                          width: 0.3,
+                          color: Colors.white30,
+                        ),
+                      ),
+                      onPressed: () {},
+                      color: kButtonColor2,
+                      textColor: Colors.white,
+                      child: Text(
+                        "=",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                          fontFamily: 'Arial',
+                          height: 1.2,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
